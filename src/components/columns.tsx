22d95061c3907ca9c3fcb0d31c30e3a1715f1791
Dennis Kimathi <63696529+DennohKim@users.noreply.github.com>
2023-05-08 10:45:47 +0300

    feat: define mock data

---
+++ src/components/columns.tsx
@@ -0,0 +1,23 @@
+export const COLUMNS = [
+  {
+    Header: "Id",
+  },
+  {
+    Header: "First Name",
+  },
+  {
+    Header: "Id",
+  },
+  {
+    Header: "Id",
+  },
+  {
+    Header: "Id",
+  },
+  {
+    Header: "Id",
+  },
+  {
+    Header: "Id",
+  },
+];
